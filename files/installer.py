98109ee0a7d367547614c258bced04ff32100265
Bill Sideris <bill88t@feline.gr>
2022-10-27 14:35:35 +0300

    remove uneccessary fstrings

--- files/installer.py
+++ files/installer.py
@@ -2,7 +2,7 @@ for filee in ["farland.lja", "farland.py"]:
     ljinux.api.var("argj", f"cp {filee} /bin/{filee}")
     ljinux.based.command.fpexecc([None, "/bin/cp.py"])
 
-ljinux.api.var("argj", f"cp font5x8.bin /etc/font5x8.bin")
+ljinux.api.var("argj", "cp font5x8.bin /etc/font5x8.bin")
 ljinux.based.command.fpexecc([None, "/bin/cp.py"])
 
 ljinux.api.var("argj", "cp adafruit_framebuf.mpy &/lib/adafruit_framebuf.mpy")
--- files/uninstaller.py
+++ files/uninstaller.py
@@ -1,8 +1,8 @@
 for filee in ["farland.lja", "farland.py"]:
-    ljinux.api.var("argj", f"rm /bin/{filee}")
+    ljinux.api.var("argj", "rm /bin/" + filee)
     ljinux.based.command.fpexecc([None, "/bin/rm.py"])
 
-ljinux.api.var("argj", f"rm /etc/font5x8.bin")
+ljinux.api.var("argj", "rm /etc/font5x8.bin")
 ljinux.based.command.fpexecc([None, "/bin/rm.py"])
 
 ljinux.api.var("argj", "rm &/lib/adafruit_framebuf.mpy")
